c336fe077fdd8016c4a463c3aa7cbe54fcac29bd
jshukla <jshukla@L0024066.internal.mcmcg.com>
2016-12-18 12:49:16 +0530

    Change-Id: I1a5147ba6665e580805e78133230f3001d3594c4

---
+++ Gmailtest/src/test/java/com/gmailsignUp/Gmailtest/SignUpProcessTest.java
@@ -0,0 +1,17 @@
+package com.gmailsignUp.Gmailtest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SignUpProcessTest {
+	
+	@Test	
+	public static void OpenBrowser(){
+		String exepath="C:\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", exepath);
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://accounts.google.com/");
+	}
+
+}
